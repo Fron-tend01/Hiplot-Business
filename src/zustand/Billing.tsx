ce98cda2901cc402204a6814e204a423b45e3bec
backhiplot <149822165+backhiplot@users.noreply.github.com>
2024-11-27 17:20:31 -0500

    asdfegr

--- src/zustand/Billing.tsx
+++ src/zustand/Billing.tsx
@@ -3,8 +3,11 @@ import { create } from 'zustand';
 
 interface StoreState {
 
-    clientToUpdate: any;
-    setClientToUpdate: (x: any) => void;
+    DataUpdate: any;
+    setDataUpdate: (x: any) => void;
+
+    modoUpdate: boolean;
+    setModoUpdate: (x: boolean) => void;
 
     division: any;
     setDivision: (x: any) => void;
@@ -12,23 +15,32 @@ interface StoreState {
     dataBillign: any;
     setDataBillign: (x: any) => void;
 
+    dates: any;
+    setDates: (x: any) => void;
+
     concepts: any;
     setConcepts: (x: any) => void;
 }
 
 export const storeBilling = create<StoreState>((set) => ({
 
-    clientToUpdate: [],
-    setClientToUpdate: (x) => set({clientToUpdate: x}),
+    DataUpdate: [],
+    setDataUpdate: (x) => set({ DataUpdate: x }),
+
+    modoUpdate: false,
+    setModoUpdate: (x) => set({ modoUpdate: x }),
 
     division: [],
-    setDivision: (x) => set({division: x}),
+    setDivision: (x) => set({ division: x }),
 
+    dates: [],
+    setDates: (x) => set({ dates: x }),
+    
     dataBillign: [],
-    setDataBillign: (x) => set({dataBillign: x}),
+    setDataBillign: (x) => set({ dataBillign: x }),
 
     concepts: [],
-    setConcepts: (x) => set({concepts: x}),
+    setConcepts: (x) => set({ concepts: x }),
 
 }));
 
