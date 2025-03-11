873b7912da2eb6ee0f146bf17cead3719a1b7bd0
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2025-03-11 14:16:59 -0500

    commit

--- src/components/rootPage/sections/sales/quotations/ModalCreate.tsx
+++ src/components/rootPage/sections/sales/quotations/ModalCreate.tsx
@@ -713,7 +713,7 @@ const ModalCreate: React.FC = () => {
                               </div>
                               <div className='td'>
                                 {article?.personalized ?
-                                  <div onClick={() => modalPersonalizedUpdate(article)} className='conept-icon'>
+                                  <div onClick={() => modalPersonalizedUpdate(article, index)} className='conept-icon'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" strokeLinejoin="round" className="lucide lucide-boxes"><path d="M2.97 12.92A2 2 0 0 0 2 14.63v3.24a2 2 0 0 0 .97 1.71l3 1.8a2 2 0 0 0 2.06 0L12 19v-5.5l-5-3-4.03 2.42Z" /><path d="m7 16.5-4.74-2.85" /><path d="m7 16.5 5-3" /><path d="M7 16.5v5.17" /><path d="M12 13.5V19l3.97 2.38a2 2 0 0 0 2.06 0l3-1.8a2 2 0 0 0 .97-1.71v-3.24a2 2 0 0 0-.97-1.71L17 10.5l-5 3Z" /><path d="m17 16.5-5-3" /><path d="m17 16.5 4.74-2.85" /><path d="M17 16.5v5.17" /><path d="M7.97 4.42A2 2 0 0 0 7 6.13v4.37l5 3 5-3V6.13a2 2 0 0 0-.97-1.71l-3-1.8a2 2 0 0 0-2.06 0l-3 1.8Z" /><path d="M12 8 7.26 5.15" /><path d="m12 8 4.74-2.85" /><path d="M12 13.5V8" /></svg>
                                   </div>
                                   :
@@ -733,7 +733,7 @@ const ModalCreate: React.FC = () => {
                                 </div>
                                 :
                                 <div className='td'>
-                                  <div className='undo-icon' onClick={() => undoConcepts(article)}>
+                                  <div className='undo-icon' onClick={() => undoConcepts(article, index)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-undo-2"><path d="M9 14 4 9l5-5" /><path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11" /></svg>
                                   </div>
                                 </div>
@@ -986,7 +986,7 @@ const ModalCreate: React.FC = () => {
                           </div>
                           <div className='td'>
                             {article?.personalized ?
-                              <div onClick={() => modalPersonalizedUpdate(article)} className='conept-icon'>
+                              <div onClick={() => modalPersonalizedUpdate(article, index)} className='conept-icon'>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" strokeLinejoin="round" className="lucide lucide-boxes"><path d="M2.97 12.92A2 2 0 0 0 2 14.63v3.24a2 2 0 0 0 .97 1.71l3 1.8a2 2 0 0 0 2.06 0L12 19v-5.5l-5-3-4.03 2.42Z" /><path d="m7 16.5-4.74-2.85" /><path d="m7 16.5 5-3" /><path d="M7 16.5v5.17" /><path d="M12 13.5V19l3.97 2.38a2 2 0 0 0 2.06 0l3-1.8a2 2 0 0 0 .97-1.71v-3.24a2 2 0 0 0-.97-1.71L17 10.5l-5 3Z" /><path d="m17 16.5-5-3" /><path d="m17 16.5 4.74-2.85" /><path d="M17 16.5v5.17" /><path d="M7.97 4.42A2 2 0 0 0 7 6.13v4.37l5 3 5-3V6.13a2 2 0 0 0-.97-1.71l-3-1.8a2 2 0 0 0-2.06 0l-3 1.8Z" /><path d="M12 8 7.26 5.15" /><path d="m12 8 4.74-2.85" /><path d="M12 13.5V8" /></svg>
                               </div>
                               :
@@ -994,7 +994,7 @@ const ModalCreate: React.FC = () => {
                             }
                           </div>
                           <div className='td'>
-                            <div className='see-icon' onClick={() => seeVerMas(index)} title='Ver mas campos'>
+                            <div className='see-icon' onClick={() => seeVerMas(index, index)} title='Ver mas campos'>
                               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" /><circle cx="12" cy="12" r="3" /></svg>
                             </div>
                           </div>
